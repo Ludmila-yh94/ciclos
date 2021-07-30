c36ffcc771c68244d2e95ce68cd2396dfcf30655
Ludmila-yh94 <82547303+Ludmila-yh94@users.noreply.github.com>
2021-07-29 22:18:02 -0300

    Create ciclos.js

---
+++ ciclos.js
@@ -0,0 +1,14 @@
+console.log ("Hola Alumnos");
+
+let miArray = ["Ludmila","Santiago","Veronica","Pablo","Sharon","Tomas","Franco","Boira","India",
+"Helga","Adriana","Marcelo"];
+
+miArray[11]
+
+for (let i = 0; i <= 11; i++) {
+        console.log(miArray[i]);
+
+if (miArray[i] == "Santiago"){
+    alert("Santiago esta en esta lista");
+}
+}
